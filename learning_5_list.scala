1bb39dc138346d2c8b67c870e34e087a4c345bf2
cuiyong <kwoo919@hotmail.com>
2016-10-14 17:51:36 +0800

    new cases

--- learning_5_list.scala
+++ learning_5_list.scala
@@ -2,6 +2,10 @@ object Hello {
     def main(args: Array[String]): Unit = {
         test_basic
         test_sort_by_match
+        test_merge_sort
+        test_orderone_funcs
+        test_adv_funcs
+        test_obj_funcs
     }
 
     def print_banner(msg: String): Unit = {
@@ -40,5 +44,108 @@ object Hello {
             case head :: rest => if (it <= head) it :: sortedlist   // 如果非空，且头部大于待插入元素 it，那么把 it 摆在第一个位置上 
                 else head :: compute(it, rest)                      // 否则，头部保持不变，并递归调用 compute，把元素 it 插入尾部的恰当位置
         }
+    }
+
+    def test_merge_sort():Unit = {
+        print_banner("test merge sort")
+
+        def mergesort[T](less:(T, T) => Boolean)(input:List[T]): List[T] = {    // curring 函数定义，参数分别是排序函数和待排序 List；并且使用了模版类型
+
+            // 两个排好序的 List 的 merge 算法
+            def merge(x:List[T], y:List[T]): List[T] = (x, y) match {
+                case (Nil, _) => y
+                case (_, Nil) => x
+                case (xh :: xt, yh :: yt) =>  if (less(xh, yh)) xh :: merge(xt, y)    // closure，这里用到了外部的 less 排序函数
+                    else yh :: merge(x, yt)
+            }
+
+            val n = input.length / 2            // 获取长度
+            if (n == 0) input
+            else {
+                val (x, y) = input splitAt n    // 或者 input.splitAt(n)，在 n 位置出分开为两个 List
+                merge(mergesort(less)(x), mergesort(less)(y))    // 对两个 List 分别递归排序后，在 merge 到一起
+            }
+        }
+
+        println("asc order: " + mergesort((x:Int, y:Int) => x < y)(List(3,7,9,5,4)))
+        val reverse_sort = mergesort((x:Int, y:Int) => x > y) _               // curring 对应的 partial 函数
+        println("desc order: " + reverse_sort(List(3,7,9,5,4)))
+    }
+
+    def test_orderone_funcs():Unit = {
+        print_banner("test order-1 functions")
+        print("List(1,2,3):::List(4,5):::List():::List(7) becomes ")
+        println(List(1,2,3):::List(4,5):::List():::List(7))    // list concatenation, output List(1, 2, 3, 4, 5, 7)
+        // 以下函数中，都是生成一个新 List, 而原 List 保持不变
+        val lst = List("1st", "2nd", "3rd")
+        println(lst.last)   // 3rd
+        println(lst.init)   // 除了 last 的前面所有部分，和 tail 的逻辑正好相反，output List(1st, 2nd)
+        println(lst.reverse)  // List(3rd, 2nd, 1st)
+        println(lst)        // 等价于 lst.toString, List(1st, 2nd, 3rd)
+        println(lst.toString)  // List(1st, 2nd, 3rd)
+        println(lst take 2)  // 等价于 lst.take(2)，output List(1st, 2nd)
+        println(lst drop 2)  // List(3rd)
+        println(lst splitAt 2)  // (List(1st, 2nd),List(3rd))
+        println(lst apply 2)  // 等价于 lst(2), output 3rd
+        println(lst(2))       // 3rd
+        println(lst.indices)  // Range(0, 1, 2)，注意返回的不是 List，而是 Range
+        println(lst.indices zip lst)  // Vector((0,1st), (1,2nd), (2,3rd))，注意返回的是 Vector
+        println(lst.zipWithIndex)     // List((1st,0), (2nd,1), (3rd,2))，而这里返回的是 List
+
+        println(lst.mkString("[", ",", "]"))   // [1st,2nd,3rd]
+        println(lst.mkString("   "))   // 1st   2nd   3rd
+        println(lst.mkString)          //  1st2nd3rd
+        val buffer = new StringBuilder 
+        lst addString(buffer, "(", ";;", ")")
+        println(buffer)     // (1st;;2nd;;3rd)，看到，上面的这段代码，和 mkString 相当
+
+        println(lst.toArray.toList)        // 注意，println(lst.toArray) 会 output 类似 [Ljava.lang.String;@5f184fc6
+        val arr = new Array[String](5)
+        lst.copyToArray(arr, 1)          // 从 arr 的 index=1 (从 0 开始) 位置开始，把 list copy 到 arr 中
+        arr.foreach(println)             // null  1st  2nd  3rd  null
+
+        val it = lst.toIterator
+        println(it.next)           // 1st
+        println(it.next)           // 2nd
+        for (r <- it) println(r)   // 3rd
+    }
+
+    def test_adv_funcs():Unit = {
+        print_banner("test advanced funcs")
+        println(List("Hello", "World") map (_.toList.reverse.mkString))    // List(olleH, dlroW)
+        println(List.range(1, 4) flatMap (i => List.range(1, i) map (j => (i, j))))   // flatMap, List.range, closure，output List((2, 1), (3, 1), (3, 2))
+
+        // 类似的，以下函数中，都是生成一个新 List, 而原 List 保持不变
+        val lst = List(1, 0, 1, 0, 2, 3)
+        println(lst partition (_ % 2 == 0))    // List( List(0, 0, 2), List(1, 1, 3) )，按条件把 List 中元素分组
+        println(lst find (_ % 2 == 0))   // 只返回满足条件的第一个 Some(0)
+        println(lst find (_ < 0))        // None
+        println(lst takeWhile (_ < 2))   // List(1,0,1,0)
+        println(lst dropWhile (_ < 2))   // List(2, 3)
+        println(lst span (_ < 1))        // List( List(), List(1, 0, 1, 0, 2, 3) )，和 partition 不同，span 遇到第一个不满足条件的位置就开始分组
+        println(lst span (_ % 2 == 1))   // List( List(1), List(0, 1, 0, 2, 3) )
+        
+        println(List(List(1,0,0), List(0,1,0), List(0,0,0)) exists (row => row forall (_ == 0)))   // List of List 中，存在 List 元素，其每个子元素都是 0, output True
+
+        // foldLeft
+        println((1 to 100).foldLeft(0)(_+_)) // curring 化参数，第一个是累积初值，第二个是 fold 方法，该方法有两个参数，按顺序分别为累积值和当前遍历值
+        println((0/:(1 to 100))(_+_))        // 和上面等价，output 5050
+        println((1 to 5).foldLeft(100)(_-_)) // res=100-1=99 | res=99-2=97 | res=97-3=94 | res=94-4=90 | res=90-5=85 (或 100 - sum(1 to 5) = 85)
+
+        // foldRight
+        println((1 to 5).foldRight(100)(_-_)) // 等价于把 List 先倒序排列，然后调用 foldLeft，而 fold 方法中的参数位置也变换一下，先遍历值后累积值
+        println(((1 to 5):\100)(_-_))         // 和上面等价，先做倒序，得到 5,4,3,2,1，然后 res=5-100=-95 | res=4-(-95)=99 | res=3-99=-96 | res=2-(-96)=98 |res=1-98=-97，搞定！
+    }
+
+    def test_obj_funcs():Unit = {
+        print_banner("test class object functions")  // 上面都是 List 类中的函数调用，下面看看 List 伴生对象中的函数
+        println(List.apply(1,2,3))    // 等价于 println(List(1,2,3))，输出就是 List(1,2,3)
+        println(List.make(3,5))       // List(5,5,5)
+        println(List.range(1,5))      // List(1,2,3,4)
+        println(List.range(9, 1, -3)) // List(9, 6, 3)
+
+        println(List(List(1,2), List(3),List(4,5)).flatten)  // List(1,2,3,4,5)
+        println(List.concat(List(), List('b'), List('c')))   // List(b, c)
+        println(List.map2(List(2, 5, 1), List(3, 4, 7))(_ * _))    // map2 对两个List 分别map，然后逐对调用方法参数；output(6, 20, 7)
     }
 }

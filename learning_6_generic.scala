d480bbfc1a8959683cb344f78c81128243d93680
cuiyong <kwoo919@hotmail.com>
2016-10-18 14:46:56 +0800

    finish generic and type cases

--- learning_6_generic.scala
+++ learning_6_generic.scala
@@ -7,6 +7,10 @@ object Hello {
         test_class_tag
         test_multiple_bound
         test_type_constraint
+        test_variance
+        test_variance_site
+        test_chain_invoke
+        test_outer_path_dep
     }
 
     def print_banner(msg: String): Unit = {
@@ -72,7 +76,7 @@ object Hello {
     def test_context_bound():Unit = {
         print_banner("test context bound")
 
-        class Pair[T : Ordering](val f: T, val s: T) {        // 这里 : 会在 context 中产生一个变量，是 Ordering 类型的
+        class Pair[T : Ordering](val f: T, val s: T) {        // 这里 : 会在 context 中产生一个变量，是 Ordering[T] 类型的
             def bigger(implicit ordered: Ordering[T]) = {     // 看到，context 自动生成的变量就是 implicit ordered
                 if(ordered.compare(f, s) > 0) f else s
             }
@@ -126,6 +130,7 @@ object Hello {
         
         def foo[T : M_A : M_B](i:T) = println("Int : M_A : M_B is OK")
         foo(2)      // 前面 implicit 定义的两个常量使得 Int : M_A : M_B 可以成功
+        // 如果没有上面两个常量，那么会报错 could not find implicit value for evidence parameter of type M_A[Int]
     }
 
     def test_type_constraint():Unit = {
@@ -133,7 +138,81 @@ object Hello {
         // A =:= B  // A 类型等同于 B 类型
         // A <:< B  // A 类型是 B 类型的子类
         def rocky[T](i:T)(implicit ev: T <:< java.io.Serializable) {    // 带一个 implicit 参数，指明 T 是 Serializable 的子类
-            print("Type T is subclass of Serializable")
+            println("Type T is subclass of Serializable")
+        }
         rocky("Spark")
+    }
+
+    def test_variance():Unit = {
+        print_banner("test variance")
+        /*
+        如果T’是T一个子类，Container[T’]应该被看做是Container[T]的子类吗？
+        协变covariant       C[T’]是 C[T] 的子类 [+T]
+        逆变contravariant   C[T] 是 C[T’]的子类 [-T]
+        不变invariant       C[T] 和 C[T’]无关   [T]
+        */
+        class Person
+        class Student extends Person
+
+        class C[+T](val args: T)
+        val value:C[Person] = new C[Student](new Student)  // 看到 Student 是 Person 的子类，那么 C[Student] 也是 C[Person] 的子类
+
+        class GParent
+        class Parent extends GParent
+        class Child extends Parent
+        class CoVar[+A]
+        class ContraVar[-A]
+        def foo(x : CoVar[Parent]) : CoVar[Parent] = identity(x)
+        def bar(x : ContraVar[Parent]) : ContraVar[Parent] = identity(x)
+
+        foo(new CoVar[Child])    // OK, CoVar[Child] 是 CoVar[Parent] 的子类，可以被 CoVar[Parent] 类型的参数使用
+        // foo(new CoVar[GParent])    // Fail，CoVar[Parent] 是 CoVar[GParent] 的子类，而子类 CoVar[Parent] 类型的参数，不能传递父类对象
+        bar(new ContraVar[GParent]) // OK，同上
+        // bar(new ContraVar[Child])  // Fail，同上
+    }
+
+    def test_variance_site():Unit = {
+        print_banner("test variance site")
+        
+        class P[+T](val f:T, val s:T) {       // 这里可以用 P[T]，功能仍然相同；而不同就是 P 类是否支持协变
+            def replaceFirst[R >: T](r : R) = new P[R](r, s)
+        } 
+
+        class P1[T](var f:T, var s:T) {              // 这里用的 var，而 P 类中不可以，否则会被认为是 contravariance
+            def replaceFirst(r:T) = new P1[T](r, s)   // 这里可以直接使用 r:T，而不用使用基类，而 P 类中不可以
+            def replaceFirst2[R >: T](r:R) = new P1[R](r, s)    // 同样可以使用基类来 replace first
+        }
+    }
+
+    def test_chain_invoke():Unit = {
+        print_banner("test chain invoke")
+        
+        class Animal {def breathe: this.type = {println("breathe"); this} }        // 返回 this.type 对象类型，Animal 或者其子类的类型
+        class Cat extends Animal {def eat : this.type = {println("eat"); this} }
+        val cat = new Cat
+        cat.breathe.eat
+    }
+    
+    def test_outer_path_dep():Unit = {
+        print_banner("test outer path dependency")
+        
+        class Outer {
+            private val x = 10
+            class Inner {
+                private val y = x + 10
+            }
+        }
+        val outer = new Outer
+        val inner = new outer.Inner   // 注意这里是 outer.Inner，不是 Outer.Inner，就是说内部类是依赖外部类对象，而不是外部类
+        val inner2 : outer.Inner = new outer.Inner   
+
+        val o1 = new Outer
+        val o2 = new Outer
+        // val i : o2.Inner = new o1.Inner    // Failed，不同外部类对象构成不同的路径，内部类依赖这个路径
+        val i : Outer#Inner = new o1.Inner    // 这个是可以的，叫类型投影，Outer#Inner 是 o1.Inner 和 o2.Inner 共同的父类
+        val i1 = new o1.Inner
+        val i2 = new o2.Inner
+        println(i1)                // Hello$Outer$1$Inner@5b480cf9，看到确实都是 Outer#Inner 的类型
+        println(i2)                // Hello$Outer$1$Inner@6f496d9f
     }
 }
